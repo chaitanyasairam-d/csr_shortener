fc823fabcd6ead9993ed6eec8ad9be552ab37870
chaitanyasairam-d <32288484+chaitanyasairam-d@users.noreply.github.com>
2018-04-11 00:37:24 +0530

    Update settings.py

--- project_url/settings.py
+++ project_url/settings.py
@@ -83,8 +83,7 @@ WSGI_APPLICATION = 'project_url.wsgi.application'
   #  }
 #}
 import dj_database_url
-
-  DATABASES = {
+DATABASES = {
       'default': dj_database_url.config(
           default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
       )
